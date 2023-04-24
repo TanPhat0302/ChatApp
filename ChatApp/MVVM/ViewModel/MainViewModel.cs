5402ad33c4e56aca0d80566fed77283d7721682e
TanPhat0302 <phatt.maii01@gmail.com>
2023-04-24 17:32:18 +0300

    Add project files.

---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.MVVM.Model;
+using ChatClient.Net;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<UserModel> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+        public RelayCommand ConnectoToServerCommand { get; set; }
+        public RelayCommand SendMessageCommand { get; set; }
+        public string Username { get; set; }
+        public string Message { get; set; }
+        private Server _server;
+        public MainViewModel()
+        {
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+            _server = new Server();
+            _server.connectedEvent += UserConnected;
+            _server.msgReceivedEvent += MessageReceived;
+            _server.userDisconnectEvent += RemoveUser;
+            ConnectoToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username), p =>!string.IsNullOrEmpty(Username));
+            SendMessageCommand = new RelayCommand(o => _server.SendMessageToServer(Message), p => !string.IsNullOrEmpty(Message));
+        }
+
+        private void RemoveUser()
+        {
+            var uid = _server.PacketReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+            Application.Current.Dispatcher.Invoke(() => Users.Remove(user));
+        }
+
+        private void MessageReceived()
+        {
+            var msg = _server.PacketReader.ReadMessage();
+            Application.Current.Dispatcher.Invoke(() => Messages.Add(msg));
+        }
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                Username = _server.PacketReader.ReadMessage(),
+                UID = _server.PacketReader.ReadMessage(),
+
+            };
+
+            if (!Users.Any(x => x.UID == user.UID))
+            {
+                Application.Current.Dispatcher.BeginInvoke(() => Users.Add(user));
+            }
+        }
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,73 @@
+﻿using ChatServer.Net.IO;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+            while (true)
+            {
+
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+
+                BroadcastConnection();
+            }
+
+
+        }
+
+        static void BroadcastConnection()
+        {
+            foreach (var user in _users) 
+            {
+                foreach (var usr in _users)
+                {
+                    var broadcasePacket = new PacketBuilder();
+                    broadcasePacket.WriteOpCode(1);
+                    broadcasePacket.WriteMessage(usr.Username);
+                    broadcasePacket.WriteMessage(usr.UID.ToString());
+                    user.ClientSocket.Client.Send(broadcasePacket.GetPacketBytes());
+                }
+            
+            }
+        }
+
+        public static void BroadcastMessage(string Message)
+        {
+            foreach (var user in _users)
+            {
+                var msgPacket = new PacketBuilder();
+                msgPacket.WriteOpCode(5);
+                msgPacket.WriteMessage(Message);
+                user.ClientSocket.Client.Send(msgPacket.GetPacketBytes());
+            }
+        }
+
+        public static void BroadcaseDisconnect(string uid)
+        {
+            var disconnectUser = _users.Where(x=>x.UID.ToString() == uid).FirstOrDefault();
+            _users.Remove(disconnectUser);
+            foreach (var user in _users)
+            {
+                var broadcasePacket = new PacketBuilder();
+                broadcasePacket.WriteOpCode(10);
+                broadcasePacket.WriteMessage(uid);
+                user.ClientSocket.Client.Send(broadcasePacket.GetPacketBytes());
+              
+            }
+            BroadcastMessage($"[{disconnectUser.Username}] Disconnected!");
+        }
+
+    }
+}
